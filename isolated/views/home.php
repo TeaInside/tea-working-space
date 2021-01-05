34fbeb614545c7292009b1ff4551fd1aff880a4f
Ammar Faizi <ammarfaizi2@gmail.com>
2021-01-05 23:33:39 +0700

    Fix group list design
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- isolated/views/home.php
+++ isolated/views/home.php
@@ -5,16 +5,23 @@
   <link rel="stylesheet" type="text/css" href="assets/css/home.css"/>
 </head>
 <body>
-</body>
   <div class="btn-cage">
-    <button>Logout</button>
+    <a href="logout.php"><button>Logout</button></a>
   </div>
   <div class="main-cage">
     <div class="h1-cage">
       <h1>Welcome to Tea Working Space</h1>
     </div>
-    <div class="list-group"></div>
-    <div class="main-handle"></div>
+    <div class="inl group-list">
+      <h3>Group List</h3>
+      <div id="group-list">
+        <div class="gl-data">
+          <div class="inlc gl-img-cg"><img class="gl-img" src=""/></div>
+          <div class="inlc gl-name">Group 1</div>
+        </div>
+      </div>
+    </div>
+    <div class="inl main-handle"></div>
   </div>
 </body>
 </html>
