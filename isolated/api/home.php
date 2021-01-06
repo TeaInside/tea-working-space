8746852330f09d32d353aa0b8727424412c76bdc
Ammar Faizi <ammarfaizi2@gmail.com>
2021-01-06 10:15:05 +0700

    Fix chat list
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- isolated/api/home.php
+++ isolated/api/home.php
@@ -10,6 +10,7 @@ if (isset($_GET["action"])) {
   switch ($_GET["action"]) {
     case "get_group_list":   $json = get_group_list();   break;
     case "get_channel_list": $json = get_channel_list(); break;
+    case "get_channel_chat_list": $json = get_channel_chat_list(); break;
     default:
       $json = ["status" => "error", "err" => "Invalid action"];
       break;
@@ -55,5 +56,30 @@ INNER JOIN ugroup_channels AS d ON a.ugroup_id = d.ugroup_id
 WHERE b.user_id = ? AND a.ugroup_id = ?"
 );
   $st->execute([$userId, $_GET["ugroup_id"]]);
+  return $st->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function get_channel_chat_list() {
+  if (!isset($_GET["ugroup_channel_id"])) {
+    exit;
+  }
+
+  if (!is_string($_GET["ugroup_channel_id"])) {
+    exit;
+  }
+
+  $userId = $_SESSION["user_id"];
+  $pdo    = DB::pdo();
+  $st     = $pdo->prepare(
+"SELECT a.ugroup_channel_msg_id AS id, a.content,
+CONCAT(b.first_name,' ',b.last_name) AS sender_name, a.created_at
+FROM ugroup_channel_msg AS a
+INNER JOIN users AS b ON b.user_id = a.sender_id
+INNER JOIN ugroup_channels AS c ON c.ugroup_channel_id = a.ugroup_channel_id
+INNER JOIN ugroups AS d ON d.ugroup_id = c.ugroup_id
+INNER JOIN ugroup_members AS e ON e.ugroup_id = d.ugroup_id
+WHERE e.user_id = ? AND a.ugroup_channel_id = ?"
+);
+  $st->execute([$userId, $_GET["ugroup_channel_id"]]);
   return $st->fetchAll(PDO::FETCH_ASSOC);
 }
--- isolated/views/home.php
+++ isolated/views/home.php
@@ -19,15 +19,29 @@
       </div>
     </div>
     <div class="inl channel-list" id="chan_cage" style="display:none;">
+      <a href="javascript:void(0);" onclick="close_channel_list();">Close</a>
       <h3>Channel List</h3>
       <div id="channel-list">
       </div>
     </div>
     <div class="inl main-handle">
-      <h3>Chat</h3>
+      <div class="main-nav">
+        <div class="mnl inl"><button onclick="open_home();">Home</button></div>
+        <div class="mnl inl"><button onclick="open_chat();">Chat</button></div>
+      </div>
+      <div class="chat-cage">
+        <div>Chat in #test-chan</div>
+        <div class="chat-chk-ls" id="chat-chk-ls">
+        </div>
+        <div class="text-cage">
+          <textarea id="chat_text" placeholder="Enter text here..."></textarea>
+          <button onclick="send_chat();">Send</button>
+        </div>
+      </div>
     </div>
   </div>
 <script type="text/javascript">
+
 function run_xhr(type, url, onload, data = null) {
   let ch = new XMLHttpRequest;
   ch.withCredentials = true;
@@ -63,12 +77,23 @@ function get_channel_list(id) {
     let r = "", i, j = JSON.parse(this.responseText);
     for (i in j) {
       r +=
-      '<div class="gl-data" onclick="handle_group_click(this, '+j[i].ugroup_channel_id+');">'+
+      '<div class="cl-data" onclick="handle_channel_click(this, '+j[i].ugroup_channel_id+');">'+
         '<div class="inlc gl-img-cg"><img class="gl-img" src="assets/img/hashtag.png"/></div>'+
         '<div class="inlc gl-name">#'+j[i].name+'</div>'+
       '</div>';
     }
     channel_list.innerHTML = r;
+  });
+}
+
+function get_channel_chat_list(id) {
+  run_xhr("GET", "home.php?action=get_channel_chat_list&ugroup_channel_id="+id, function () {
+    let json = JSON.parse(this.responseText);
+    let i, r = "";
+    for (i in json) {
+      r +='<div class="chat-ls"><p><b>'+json[i].sender_name+': </b>Hello World!</p></div>'
+    }
+    get_id("chat-chk-ls").innerHTML = r;
   });
 }
 
@@ -79,6 +104,27 @@ function handle_group_click(el, id) {
   }
   el.style["background-color"] = "#d4f4cb";
   get_channel_list(id);
+}
+
+function handle_channel_click(el, id) {
+  let x = document.getElementsByClassName("cl-data");
+  for (let i = 0; i < x.length; i++) {
+    x[i].style["background-color"] = "#fff";
+  }
+  el.style["background-color"] = "#d4f4cb";
+  get_channel_chat_list(id);
+}
+
+function open_home() {
+
+}
+
+function open_chat() {
+
+}
+
+function close_channel_list() {
+  get_id("chan_cage").style.display = "none";
 }
 
 get_group_list();
